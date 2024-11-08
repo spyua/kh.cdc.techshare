7bfab50425a3eb7099a537d9b46eece43e28c90c
pal2097 <pal2097@gmail.com>
2024-11-08 08:21:02 +0800

    feat:update sample

---
+++ sample.code.service/LifeTimeSample/manual/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample.code.service.LifeTimeSample.manual
+{
+    public class Logger
+    {
+        private static Logger _instance;
+        private static readonly object _lock = new object();
+
+        // 私有建構子，防止外部直接實例化
+        private Logger() { }
+
+        // 提供單例存取的靜態方法
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Logger();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: {message}");
+        }
+    }
+}
